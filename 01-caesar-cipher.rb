b3b4197cd18d6c0dd6011a175c22cbc54b1eb44e
branko-stankovic <branko.stankovic@gmail.com>
2021-12-06 01:37:10 +0100

    Refactor caesar's cipher

--- 01-caesar-cipher.rb
+++ 01-caesar-cipher.rb
@@ -1,21 +1,20 @@
-def caesar_cipher(plaintext, num)
-    plaintext = plaintext.split("")
-  
-    plaintext.each do |char|
-      if char >= "A" && char <= "Z"
-        print (65 + ((char.ord - 65 + num) % 26)).chr
-      elsif char >= "a" && char <= "z"
-        print (97 + ((char.ord - 97 + num) % 26)).chr
-      else
-        print char
-      end
+def shift(letter, num)
+  if letter >= "A" && letter <= "Z"
+      (65 + ((letter.ord - 65 + num) % 26)).chr
+    elsif letter >= "a" && letter <= "z"
+      (97 + ((letter.ord - 97 + num) % 26)).chr
+    else
+      letter
     end
-    print "\n"
-  end
-  
-  caesar_cipher("ABCD", 27)
-  caesar_cipher("ABCD", -1)
-  caesar_cipher("Abcd", 6)
-  caesar_cipher("xyza", 676)
-  caesar_cipher("Hello, World!", 1)
-  caesar_cipher("What a string!", 5)
+end
+
+def caesar_cipher(plaintext, num)
+  puts plaintext.split("").map{|char| shift(char, num)}.join("")
+end
+
+caesar_cipher("ABCD", 27)
+caesar_cipher("ABCD", -1)
+caesar_cipher("Abcd", 6)
+caesar_cipher("xyza", 676)
+caesar_cipher("Hello, World!", 1)
+caesar_cipher("What a string!", 5)
